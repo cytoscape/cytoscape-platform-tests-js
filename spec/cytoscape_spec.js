8464f05575dc13c834f1adf85a06fe8ea8a18233
Unknown <dotasek.dev@gmail.com>
2018-09-19 09:21:40 -0700

    Added 2 tests, moved button

--- spec/cytoscape_spec.js
+++ spec/cytoscape_spec.js
@@ -1,11 +1,28 @@
 describe("Cytoscape", function() {
 
+  beforeAll(function() {
+    //console.log(window.DATA['log'])
+  });
 
   beforeEach(function() {
 
   });
 
-  it("should print set variable", function() {
-    expect(testValue).toEqual("rock");
+  it("check status", function() {
+    var testValue = window.DATA.responses["status"]
+    var expectedValue = { 
+      allAppsStarted: true,
+      apiVersion: 'v1'
+    }
+    expect(testValue).toEqual(expectedValue);
+  });
+
+  it("check version", function() {
+    var testValue = window.DATA.responses["version"]
+    var expectedValue = { 
+      cytoscapeVersion: '3.7.0-SNAPSHOT',
+      apiVersion: 'v1'
+    }
+    expect(testValue).toEqual(expectedValue);
   });
 });
