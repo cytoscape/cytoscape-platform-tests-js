f2c4223fc05cc16194dfff2afb19279f60da0413
Brett Settle <brettjsettle@gmail.com>
2018-09-19 18:12:26 -0700

    First pass lint

--- js/cycaller.js
+++ js/cycaller.js
@@ -1,9 +1,9 @@
 class CyRESTInstance {
   constructor(base_url, port) {
-    if (base_url === undefined){
+    if (base_url === undefined) {
       base_url = CyRESTInstance.BASE_URL
     }
-    if (port === undefined){
+    if (port === undefined) {
       port = CyRESTInstance.PORT
     }
     this.port = port
@@ -32,14 +32,14 @@ class CyCaller {
     // ""
     // "Constructor remembers CyREST location and NDEx credentials"
     // ""
-    if (cy_rest_instance === undefined){
+    if (cy_rest_instance === undefined) {
       cy_rest_instance = new CyRESTInstance()
     }
     this.cy_rest_instance = cy_rest_instance
     this.log = undefined
   }
-  
-  setLogCallBack(callback){
+
+  setLogCallBack(callback) {
     this.log = callback
   }
 
@@ -50,9 +50,9 @@ class CyCaller {
     const return_json = CyCaller._return_json(result)
 
     errors = return_json["errors"]
-    if (len(errors) === 0){
+    if (len(errors) === 0) {
       return return_json["data"]
-    }else {
+    } else {
       throw new Exception(errors)
     }
   }
@@ -62,14 +62,14 @@ class CyCaller {
     // ""
     return JSON.parse(result)
   }
-  
-  request(method, url, data, callback){
+
+  request(method, url, data, callback) {
     const Http = new XMLHttpRequest();
     Http.open(method, url);
     Http.setRequestHeader('Content-type', 'application/json')
     Http.setRequestHeader('Accept', 'application/json')
     Http.send(data);
-    Http.onreadystatechange=(e)=>{
+    Http.onreadystatechange = (e) => {
       if (Http.readyState == 4 && Http.status == 200) {
         callback(Http.responseText)
       }
@@ -82,59 +82,59 @@ class CyCaller {
     // ""
     const hasData = (data instanceof Array && data.length > 0) || (data instanceof Object && Object.keys(data).length !== 0)
     var url = this.cy_rest_instance.base_url + ":" + this.cy_rest_instance.port + endpoint;
-    if (this.log){
+    if (this.log) {
       this.log(http_method.toUpperCase() + " " + url, "call")
-      if (hasData){
+      if (hasData) {
         this.log("data=" + JSON.stringify(data))
       }
     }
-    
-    if (hasData){
+
+    if (hasData) {
       data = JSON.stringify(data);
-    }else{
+    } else {
       data = null;
     }
-    
+
     this.request(http_method,
       url,
       data,
       callback)
   }
 
-  post(endpoint, data, callback=console.log) {
+  post(endpoint, data, callback = console.log) {
     // ""
     // "Execute a REST call using POST"
     // ""
     return this._execute("post", endpoint, data, callback)
   }
-  put(endpoint, data, callback=console.log) {
+  put(endpoint, data, callback = console.log) {
     // ""
     // "Execute a REST call using PUT"
     // ""
     return this._execute("put", endpoint, data, callback)
   }
-  get(endpoint, callback=console.log) {
+  get(endpoint, callback = console.log) {
     // ""
     // "Execute a REST call using GET"
     // ""
     return this._execute("get", endpoint, {}, callback)
   }
-  delete(endpoint, data, callback=console.log) {
+  delete(endpoint, data, callback = console.log) {
     // ""
     // "Execute a REST call using DELETE"
     // ""
     return this._execute("delete", endpoint, data, callback)
   }
-  
+
   load_file_from_url(url, callback) {
-    const body = [{"source_location": url,"source_method": "GET"}]
-    this.post("/v1/networks?source=url&format=cx", body, function(r){
+    const body = [{ "source_location": url, "source_method": "GET" }]
+    this.post("/v1/networks?source=url&format=cx", body, function (r) {
       const data = JSON.parse(r)
       const suid = data[0]['networkSUID']
       callback(suid)
     })
   }
-  
+
   get_network_suid(callback) {
     this.get("/v1/networks", callback)
   }
--- js/main.js
+++ js/main.js
@@ -1,89 +1,87 @@
-window.DATA = {'log': [], 'responses': {}}
-let currentSlide = undefined;
-
+window.DATA = { 'log': [], 'responses': {} }
 
 const logArea = document.getElementsByTagName('textarea')[0]
 
-function tester(slide){
-  addResponse(slide.id, {"appVersion": window.navigator['appVersion']})
-  showControls();
+function tester (slide) {
+  addResponse(slide.id, { 'appVersion': window.navigator['appVersion'] })
+  showControls()
 }
 
 /* SLIDES */
-function close_session(slide){
-  cyCaller.delete("/v1/session", {}, function(r){
-    showControls();
-    return;
+function close_session (slide) {
+  cyCaller.delete('/v1/session', {}, function (r) {
+    showControls()
   })
 }
 
-function galfiltered(slide){
-  const url = "http://chianti.ucsd.edu/~bsettle/galFiltered.cx"
-  cyCaller.load_file_from_url(url, function(suid){
-    this.log("Loaded galfiltered with SUID " + suid)
-    var edges = []
-    cyCaller.get("/v1/networks/" + suid + "/edges", function(edges){
+function galfiltered (slide) {
+  const url = 'http://chianti.ucsd.edu/~bsettle/galFiltered.cx'
+  cyCaller.load_file_from_url(url, function (suid) {
+    this.log(slide, 'Loaded galfiltered with SUID ' + suid)
+    cyCaller.get('/v1/networks/' + suid + '/edges', function (edges) {
       edges = JSON.parse(edges)
-      log("Edges in galfiltered = " + edges.length, "response")
-      addResponse(slide.id, )
-      
-      const check = slide.getElementsByClassName("edgeCountMatches")[0]
-      check.labels[0].innerText = "Edge count is " + edges.length + "?"
-      cyCaller.get("/v1/networks/" + suid + "/nodes", function(nodes){
+      log(slide, 'Edges in galfiltered = ' + edges.length, 'response')
+      addResponse(slide.id)
+
+      const check = slide.getElementsByClassName('edgeCountMatches')[0]
+      check.labels[0].innerText = 'Edge count is ' + edges.length + '?'
+      cyCaller.get('/v1/networks/' + suid + '/nodes', function (nodes) {
         nodes = JSON.parse(nodes)
-        log("Nodes in galfiltered = " + nodes.length, "response")
-        addResponse(slide.id, {'cyrestNodeCount': nodes.length,
-                                    'cyrestEdgeCount': edges.length})
-        
-        const check = slide.getElementsByClassName("nodeCountMatches")[0]
-        check.labels[0].innerText = "Node count is " + nodes.length + "?"
-        showControls();
-      });
-    });
-  });
+        log(slide, 'Nodes in galfiltered = ' + nodes.length, 'response')
+        addResponse(slide.id, {
+          'cyrestNodeCount': nodes.length,
+          'cyrestEdgeCount': edges.length
+        })
+
+        const check = slide.getElementsByClassName('nodeCountMatches')[0]
+        check.labels[0].innerText = 'Node count is ' + nodes.length + '?'
+        showControls()
+      })
+    })
+  })
 }
 
-function diffusion(slide){
+function diffusion (slide) {
   const select_nodes = (suid, node_suids) => {
-    cyCaller.put("/v1/networks/"+ suid + "/nodes/selected", node_suids, function(){
-      cyCaller.post("/diffusion/v1/currentView/diffuse", {}, () => {
+    cyCaller.put('/v1/networks/' + suid + '/nodes/selected', node_suids, function () {
+      cyCaller.post('/diffusion/v1/currentView/diffuse', {}, () => {
         showControls()
       })
     })
   }
-  const url = "http://chianti.ucsd.edu/~bsettle/galFiltered.cx"
-  cyCaller.load_file_from_url(url, function(suid){
-    cyCaller.get("/v1/networks/" + suid + "/tables/defaultnode/rows", function(r){
+  const url = 'http://chianti.ucsd.edu/~bsettle/galFiltered.cx'
+  cyCaller.load_file_from_url(url, function (suid) {
+    cyCaller.get('/v1/networks/' + suid + '/tables/defaultnode/rows', function (r) {
       const rows = JSON.parse(r)
-      for (var i in rows){
-        if (rows[i]['COMMON'] === 'RAP1'){
+      for (var i in rows) {
+        if (rows[i]['COMMON'] === 'RAP1') {
           select_nodes(suid, [rows[i]['SUID']])
-          return;
+          return
         }
       }
-    })
-  });
-}
-
-function layout(slide){
-  const url = "http://chianti.ucsd.edu/~bsettle/galFiltered.cx"
-  cyCaller.load_file_from_url(url, function(suid){
-    cyCaller.get("/v1/apply/layouts/circular/" + suid, 
-    function(){
-      showControls();
     })
   })
 }
 
-function handleCYS(area, files){
-  addResponse("session_save", {"file_size": files[0].size})
-  area.style.backgroundColor = "#669166"
+function layout (slide) {
+  const url = 'http://chianti.ucsd.edu/~bsettle/galFiltered.cx'
+  cyCaller.load_file_from_url(url, function (suid) {
+    cyCaller.get('/v1/apply/layouts/circular/' + suid,
+      function () {
+        showControls()
+      })
+  })
 }
 
-function initDropArea(){
+function handleCYS (area, files) {
+  addResponse('session_save', { 'file_size': files[0].size })
+  area.style.backgroundColor = '#669166'
+}
+
+function initDropArea () {
   const dropArea = document.getElementById('drop-area')
-  dropArea.style.visibility="visible";
-  
+  dropArea.style.visibility = 'visible'
+
   ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
     dropArea.addEventListener(eventName, preventDefaults, false)
   })
@@ -100,162 +98,162 @@ function initDropArea(){
     dropArea.addEventListener(eventName, unhighlight, false)
   })
 
-  function highlight(e) {
+  function highlight (e) {
     dropArea.classList.add('highlight')
   }
 
-  function unhighlight(e) {
+  function unhighlight (e) {
     dropArea.classList.remove('highlight')
   }
   dropArea.addEventListener('drop', handleDrop, false)
 
-  function handleDrop(e) {
+  function handleDrop (e) {
     let dt = e.dataTransfer
     let files = dt.files
 
     handleCYS(dropArea, files)
   }
-  
-  return dropArea;
+
+  return dropArea
 }
 
-function session_save(slide){
-  const url = "http://chianti.ucsd.edu/~bsettle/galFiltered.cx"
-  cyCaller.load_file_from_url(url, function(suid){
-    cyCaller.post("/v1/session?file=", {}, (loc) => {
+function session_save (slide) {
+  const url = 'http://chianti.ucsd.edu/~bsettle/galFiltered.cx'
+  cyCaller.load_file_from_url(url, function (suid) {
+    cyCaller.post('/v1/session?file=', {}, (loc) => {
       loc = JSON.parse(loc)['file']
       const text = slide.getElementsByClassName('text')[0]
-      text.innerText = "Saved session file to " + loc + ".cys"
-      
-      initDropArea();
-      showControls();
+      text.innerText = 'Saved session file to ' + loc + '.cys'
+
+      initDropArea()
+      showControls()
     })
- })
+  })
 }
 
-function runjasmine(slide){
-  log(JSON.stringify(window.DATA['responses']))
-  window.runtests();
+function runjasmine (slide) {
+  log(slide, JSON.stringify(window.DATA['responses']))
+  window.runtests()
 }
 
 /* HELPERS */
-function addResponse(name, data){
-  if (!window.DATA.responses.hasOwnProperty(name)){
+function addResponse (name, data) {
+  if (!window.DATA.responses.hasOwnProperty(name)) {
     window.DATA.responses[name] = {}
   }
   window.res = Object.assign(window.DATA.responses[name], data)
 }
 
-function log(message, type="info", clear=false){
-  let context = "log";
-  if (currentSlide && currentSlide.id){
-    context = currentSlide.id
+function log (slide, message, type = 'info', clear = false) {
+  let context = 'log';
+  if (slide && slide.id) {
+    context = slide.id
   }
-  
-  const line = context + "::" + type + " - " + message
+
+  const line = context + '::' + type + ' - ' + message
   window.DATA['log'].push(line)
-  logArea.innerHTML = window.DATA['log'].join("\n")
-  logArea.scrollTop = logArea.scrollHeight;
-  if (currentSlide){
-    const log = currentSlide.getElementsByClassName("slide_log")[0]
-    if (log){
-      if (clear){
-        log.innerHTML = ""
+  logArea.innerHTML = window.DATA['log'].join('\n')
+  logArea.scrollTop = logArea.scrollHeight
+  if (slide) {
+    const log = slide.getElementsByClassName('slide_log')[0]
+    if (log) {
+      if (clear) {
+        log.innerHTML = ''
       }
-      log.innerHTML += message + "\n"
-      log.scrollTop = log.scrollHeight;
+      log.innerHTML += message + '\n'
+      log.scrollTop = log.scrollHeight
     }
   }
 }
 
-function buildInput(n){
-  let entry = ""
-  if (n['type'] === 'checkbox'){
-    entry = "<input type='checkbox' id='" + n['id'] + "' class='" + n['id'] + "'/>" + 
-    "<label for='" + n['id'] + "'>" + n['text'] + "</label>"
-  }else if (n['type'] === 'text'){
-    entry = "<label for='" + n['id'] + "'>" + n['text'] + "</label>"
-      + "<input type='text' id='" + n['id'] + "'' name='" + n['id'] + "'/>"
-  }else {
+function buildInput (n) {
+  let entry = ''
+  if (n['type'] === 'checkbox') {
+    entry = "<input type='checkbox' id='" + n['id'] + "' class='" + n['id'] + "'/>" +
+      "<label for='" + n['id'] + "'>" + n['text'] + '</label>'
+  } else if (n['type'] === 'text') {
+    entry = "<label for='" + n['id'] + "'>" + n['text'] + '</label>' +
+      "<input type='text' id='" + n['id'] + "'' name='" + n['id'] + "'/>"
+  } else {
     entry = "<input type='" + n['type'] + "' id='" + n['id'] + "' class='" + n['id'] + "'/>"
   }
-  return "<div class='entry'>" + entry + "</div>"
+  return "<div class='entry'>" + entry + '</div>'
 }
 
-function buildSlide(options, container){
-  var slide = '<h3>' + options.title + "</h3>";
-  if (options.text){
-    slide += "<p class='text'>" + options.text + "</p>"
+function buildSlide (options, container) {
+  var slide = '<h3>' + options.title + '</h3>'
+  if (options.text) {
+    slide += "<p class='text'>" + options.text + '</p>'
   }
-  if (!options.hasOwnProperty('log') || options["log"] !== 'false'){
-    slide += '<textarea class="slide_log" readonly></textarea>';
+  if (!options.hasOwnProperty('log') || options['log'] !== 'false') {
+    slide += '<textarea class="slide_log" readonly></textarea>'
   }
   slide += '<div class="entries" id="entries">'
-  if (options.inputs){
-    for (var n in options.inputs){
+  if (options.inputs) {
+    for (var n in options.inputs) {
       slide += buildInput(options.inputs[n])
     }
   }
-  slide += "</div>"
+  slide += '</div>'
   // slide += "<p class='result'></p>"
-  container.innerHTML = slide;
+  container.innerHTML = slide
 }
 
-function clearSession(callback, slide){
-  cyCaller.delete("/v1/session", {}, function(r){
-    callback(slide);
+function clearSession (callback, slide) {
+  cyCaller.delete('/v1/session', {}, function (r) {
+    callback(slide)
   })
 }
 
-function call(slide){
+function call (slide) {
   const funcs = {
-    "tester": tester,
-    "close_session": close_session,
-    "galfiltered": galfiltered,
-    "diffusion": diffusion,
-    "layout": layout,
-    "session_save": session_save,
-    // "galfiltered": (v) => { clearSession(galfiltered, v) },
-    // "diffusion": (v) => { clearSession(diffusion, v) },
-    // "layout": (v) => { clearSession(layout, v) },
-    // "session_save": (v) => { clearSession(session_save, v) },
-    "runjasmine": runjasmine
+    'tester': tester,
+    'close_session': close_session,
+    'galfiltered': galfiltered,
+    'diffusion': diffusion,
+    'layout': layout,
+    'session_save': session_save,
+    // 'galfiltered': (v) => { clearSession(galfiltered, v) },
+    // 'diffusion': (v) => { clearSession(diffusion, v) },
+    // 'layout': (v) => { clearSession(layout, v) },
+    // 'session_save': (v) => { clearSession(session_save, v) },
+    'runjasmine': runjasmine
   }
-  
-  log("Starting test", "init", true)
-  if (funcs.hasOwnProperty(slide.id)){
-    Reveal.configure({controls: false})
+
+  log(slide, 'Starting test', 'init', true)
+  if (funcs.hasOwnProperty(slide.id)) {
+    Reveal.configure({ controls: false })
     funcs[slide.id](slide)
-    setTimeout(() => { Reveal.configure({controls: true}) }, 5000)
-  }else {
+    setTimeout(() => { Reveal.configure({ controls: true }) }, 5000)
+  } else {
     showControls()
   }
 }
 
-function save_answers(slide){
-  if (!slide){
-    return;
+function save_answers (slide) {
+  if (!slide) {
+    return
   }
-  const inputs = slide.getElementsByTagName("input")
-  for (let i = 0; i < inputs.length; i++){
-    const inp = inputs[i];
-    if (inp.type === 'file'){
-      continue;
+  const inputs = slide.getElementsByTagName('input')
+  for (let i = 0; i < inputs.length; i++) {
+    const inp = inputs[i]
+    if (inp.type === 'file') {
+      continue
     }
-    const id = slide.id;
+    const id = slide.id
     const value = {
-      "checkbox": inp.checked,
-      "text": inp.value
+      'checkbox': inp.checked,
+      'text': inp.value
     }[inp.type]
-    const obj = {[inp.id] : value}
+    const obj = { [inp.id]: value }
     addResponse(id, obj)
   }
 }
 
-function showControls(vis=true){
+function showControls (vis = true) {
   // TODO: How to handle errors that prevent this from being called?
   // A timer was placed in the slide call func
-  Reveal.configure({controls: vis})
+  Reveal.configure({ controls: vis })
 }
 
 Reveal.initialize({
@@ -264,36 +262,31 @@ Reveal.initialize({
     { src: 'plugin/markdown/marked.js' },
     { src: 'plugin/markdown/markdown.js' },
     { src: 'plugin/notes/notes.js', async: true },
-    { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
-  ], 
+    { src: 'plugin/highlight/highlight.js', async: true, callback: function () { hljs.initHighlightingOnLoad(); } }
+  ],
   anything: [{
-    className: "cyrest",
-    defaults: {"name": "start", "title": "Cytoscape Testing"},
-    initialize: (function(container, options){
-      if (!options){
+    className: 'cyrest',
+    defaults: { 'name': 'start', 'title': 'Cytoscape Testing' },
+    initialize: function (container, options) {
+      if (!options) {
         options = {}
       }
       buildSlide(options, container)
-      if (container.id === "init"){
-        currentSlide = container;
-      }
-    })
+    }
   }],
-  controlsBackArrows: "hidden",
+  controlsBackArrows: 'hidden',
   controlsTutorial: false,
-  progress: true, //TODO: change to false to prevent backstepping
+  progress: true, // TODO: change to false to prevent backstepping
   keyboard: false,
-  overview: false,
-});
+  overview: false
+})
 
-Reveal.addEventListener( 'slidechanged', function( event ) {
-	// event.previousSlide, event.currentSlide, event.indexh, event.indexv
-  console.log(event)
-  currentSlide = event.currentSlide;
+Reveal.addEventListener('slidechanged', function (event) {
+  // event.previousSlide, event.currentSlide, event.indexh, event.indexv
   save_answers(event.previousSlide)
   call(event.currentSlide)
-});
+})
 
-const cyCaller = new CyCaller();
+const cyCaller = new CyCaller()
 cyCaller.setLogCallBack(log)
-log("Started Cytoscape Testing")
+log(null, 'Started Cytoscape Testing')
