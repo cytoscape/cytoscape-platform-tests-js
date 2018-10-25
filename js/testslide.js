93954d17f0b85aa55de4b2bee99f4aa8d094d8be
Kemari <kemaric@gmail.com>
2018-10-25 01:10:24 -0400

    Added base slide class design.

---
+++ js/testslide.js
@@ -0,0 +1,54 @@
+
+class BaseTestSlide {
+    static get id() {
+        return TestSlide._id;
+    }
+    get uid() {
+        return this._uid;
+    }
+    set uid(newId){
+        this._uid = newId;
+    }
+    constructor() {
+        if (!TestSlide._id){  TestSlide._id = 0; }  
+        TestSlide._id++;
+        this._uid = `${this.constructor.name}-${TestSlide._id}`;
+    }
+    toString(){
+        return this.uid;
+    }
+}
+
+class TestSlide extends BaseTestSlide {
+    constructor(){
+        super();
+        this._label = "";
+        this.question = "";
+        this.options = [];
+        this.steps = [];
+        this.answer = "";
+        this.userResponse = "";
+    }
+
+    toString(){
+        return `\n`+
+        `\tName: ${this.uid}\n`+
+        `\tCategory: ${this._label}\n `+
+        `\tQuestion: ${this.question}\n `+
+        `\tOptions: ${this.options}\n `+
+        `\tSteps: ${this.steps}\n `+
+        `\tUser Response: ${this.userResponse}\n `+
+        `\tCorrect Response: ${this.answer}\n `+
+        `\tResult... ${this.userResponse == this.answer? "Pass!":"Fail!"}\n `+
+        `\n`
+        ;
+    }
+}
+
+function main(){
+    let slides =[new TestSlide(), new TestSlide(), new TestSlide()];
+    console.log("Making slide(s)...",...slides.toString());
+
+}
+
+main();
