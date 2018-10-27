9e65732f0d4bff0afdf2e70ca7bb15d8bf396e50
Kemari <kemaric@gmail.com>
2018-10-27 16:27:36 -0400

    Adds properties for the Slide model for operations and more.

--- js/testslide.js
+++ js/testslide.js
@@ -1,54 +1,143 @@
-
-class BaseTestSlide {
-    static get id() {
-        return TestSlide._id;
+const __ = require("lodash");
+/**
+ * The base class for Slides.
+ */
+class BaseSlide {
+    constructor(label) {
+        if (!BaseSlide._id) { BaseSlide._id = 0; }
+        BaseSlide._id++;
+        this._uid = `${this.constructor.name}-${BaseSlide._id}`;
+        this._label = label;
     }
+    /**
+     * The Id number of the slide instances created.
+     */
+    static get id() {
+        return BaseSlide._id;
+    }
+    /**
+     * Returns the unique identifier for the slide instance
+     */
     get uid() {
         return this._uid;
     }
-    set uid(newId){
-        this._uid = newId;
-    }
-    constructor(label) {
-        if (!TestSlide._id){  TestSlide._id = 0; }  
-        TestSlide._id++;
-        this._uid = `${this.constructor.name}-${TestSlide._id}`;
-        this._label = label;
-    }
-    toString(){
-        return this.uid;
+    /**
+     * Returns the unique idenfing ID of the slide instance.
+     */
+    toString() {
+        return this._uid;
     }
 }
-
-class TestSlide extends BaseTestSlide {
-    constructor(){
+/**
+ * Class to hold the model of the Test Case. It holds the necessary parts of a test case
+ * including, but not limited to: the question, a series of steps, test operations, and
+ * the user answer.
+ */
+class TestSlide extends BaseSlide {
+    constructor() {
         super();
+        /**
+         * The question or text for the slide.
+         */
         this.question = "";
+        /**
+         * [Optional] Options for the slide. If it's a test, then it would be possible options for the 
+         * question's answer.
+         */
         this.options = [];
+        /**
+         * Breadcrum steps for the slide. These can be instructions for the user to follow.
+         */
         this.steps = [];
+        /**
+         * [Optional] The answer to the question. 
+         */
         this.answer = "";
+        /**
+         * The user's response to the slide's question.
+         */
         this.userResponse = "";
+        /**
+         * [Optional] The function operations that the slide will perform. 
+         */
+        this.operations = [];
     }
 
 
-    toString(){
-        return `\n`+
-        `\tName: ${this.uid}\n`+
-        `\tCategory: ${this._label}\n `+
-        `\tQuestion: ${this.question}\n `+
-        `\tOptions: ${this.options}\n `+
-        `\tSteps: ${this.steps}\n `+
-        `\tUser Response: ${this.userResponse}\n `+
-        `\tCorrect Response: ${this.answer}\n `+
-        `\tResult... ${this.userResponse == this.answer? "Pass!":"Fail!"}\n `+
-        `\n`
-        ;
+
+
+
+    toString() {
+        return `\n` +
+            `\tName: ${this.uid}\n` +
+            `\tCategory: ${this._label}\n ` +
+            `\tQuestion: ${this.question}\n ` +
+            `\tOptions: ${this.options}\n ` +
+            `\tSteps: ${this.steps}\n ` +
+            `\tUser Response: ${this.userResponse}\n ` +
+            `\tCorrect Response: ${this.answer}\n ` +
+            `\tResult... ${this.userResponse == this.answer ? "Pass!" : "Fail!"}\n ` +
+            `\n`
+            ;
+    }
+
+    /**
+     * Starts the test slide and it's operations.
+     */
+    run() {
+        if (__.isEmpty(this.question)) {
+            return;
+        }
+        console.debug(`Starting ${this.uid}...`);
+        this.runOperations();
+    }
+
+    /**
+     * Runs the operations for the current slide.
+     */
+    runOperations() {
+        if (!__.isArray(this.operations)) {
+            return;
+        }
+        // Running each operation in the test slide.
+        let currentOperation = 0;
+        console.debug(`\t\tStarting operations for ${this.uid}...`);
+        __.each(this.operations ,operation => {
+            try {
+                currentOperation++;
+                console.debug(`\t\t\tAttempting to run operation ${currentOperation}...`);
+                operation();
+                console.debug(`\t\t\tFinished running operation ${currentOperation}.`);
+            } catch (err) {
+                console.error(`\t\t\tTest operation ${currentOperation} failed.`, err);
+                return err;
+            }
+        });
+    }
+
+    /**
+     * Adds the passed in function operation to the TestSlide's list of operations.
+     * These would get added to the test instance to run.
+     * @param {*} operation 
+     */
+    addTestOperation(operation) {
+        if (__.isFunction(operation)) {
+            this.operations.push(operation);
+        } else {
+            console.error("An invalid operation was attepted to be added.");
+        }
     }
 }
 
-function main(){
-    let slides =[new TestSlide(), new TestSlide(), new TestSlide()];
-    console.log("Making slide(s)...",...slides.toString());
+function main() {
+    let slides = [new TestSlide(), new TestSlide(), new TestSlide()];
+    slides[0].question = "Starting Test";
+    slides[slides.length - 1].question = "Ending Test";
+    // console.log("Making slide(s)...", ...slides.toString());
+    __.each(slides,slide => {
+        console.log("Starting slide...", ...slide.uid);
+        slide.run();
+    });
 
 }
 
