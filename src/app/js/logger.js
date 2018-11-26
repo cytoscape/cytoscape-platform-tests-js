39fa3647fb0309ab454a238fb261aaa01807b0f4
Bernard Udu <budu@student.umuc.edu>
2018-11-25 21:14:48 -0800

    Committing cylogger class

--- src/app/js/logger.js
+++ src/app/js/logger.js
@@ -4,32 +4,30 @@
 
 class CyLogger{
     constructor(){
-        this.logItems = [];
+        this.logItems = [new CyLogItem()]
         this.log = undefined
 
         function info(message, context){
-            return log("this is an info log "+message, context)            
+            console.log(message, context)            
         }
         function debug(message, context){
-            return log(message, context)            
+            console.log(message, context)            
         }
         function warn(message, context){
-            return log(message, context)
+            console.log(message, context)
         }
         function error(message, context){
-            return log(message, context)
-        }
-        function log(message, context = 'info and error') {
-            const line = context + ' :::: ' + message
-            this.logItems.push(line)
-            const log = document.getElementById('log')
-            log.innerHTML = this.logItems.join('\n')
-            log.scrollTop = log.scrollHeight
+            console.log(message, context)
         }
         function export2jira(session){}
     } 
-    setLogCallBack (callback) {
-        this.log = callback
+
+    log(message, context = 'info') {
+        const line = context + ' :::: ' + message
+        this.logItems.push(line)
+        const log = document.getElementById('log')
+        log.innerHTML = this.logItems.join('\n')
+        log.scrollTop = log.scrollHeight
     }
 
 }
@@ -50,6 +48,25 @@ class CyLogItem{
     this.test_date = Date.now()
     this.endDate = Date.now()
     this.startDate = Date.now()
+    }
+    toString() {
+        return `\n` +
+            `\tName: ${this.uid}\n` +
+            `\tCategory: ${this.label}\n ` +
+            `\tQuestion: ${this.question}\n ` +
+            `\tOptions: ${this.options}\n ` +
+            `\tSteps: ${this.steps}\n ` +
+            `\Expected Response: ${this.expectedAnswer}\n ` +
+            `\tUser Response: ${this.userResponse}\n ` +
+            `\tOperations: ${this.operations}\n ` +
+            `\Data: ${this.data}\n` +
+            `\Context: ${this.context}\n ` +
+            `\Message: ${this.message}\n ` +
+            `\Session ID: ${this.sessionID}\n ` +
+            `\Test Date: ${this.test_date}\n ` +
+            `\End Date: ${this.endDate}\n ` +
+            `\tStart Date: ${this.startDate}\n ` +
+            `\n`;
     }
       
 }
